132b47997994cec29f6ea1ba02ba58a59b513ca3
kj <koreanjesus2011@gmail.com>
2026-02-19 21:46:10 -0500

    Moved Player class into containers

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ def main():
 
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player((SCREEN_WIDTH //2), (SCREEN_HEIGHT //2))
 
     while True:
@@ -20,8 +25,11 @@ def main():
                 return
         
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        #player.update(dt)
+        updatable.update(dt)
+        #player.draw(screen)
+        for drawables in drawable:
+            drawables.draw(screen)
         pygame.display.flip()
 
         #Limit the framerate to 60 FPS
